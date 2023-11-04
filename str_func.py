c745e55ae09db25240db63813f2e15729c91b7c1
bmamaxim <bma@gmail.com>
2023-11-04 23:36:40 +0500

    ttlfunc

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def str_upper(word: str):
     return word.upper()
+
+
+def get_title(words: str) -> str:
+    """
+    Каждое новое слово с большой буквы
+    :param words: str
+    :return: str
+    """
+    return words.title()
